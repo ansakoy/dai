d1367c3e64e2a67c8d3efbc1d1c2674012dcdf37
ansakoy <ansakoy@gmail.com>
2019-01-24 19:01:04 +0300

    finishing assignment 3

--- dai1_dmv/assignment3.py
+++ dai1_dmv/assignment3.py
@@ -52,36 +52,23 @@ def main():
 
     # Convert Unknown and Other to NaN
     # data[ANIMALS_MAP[CODE]] = data[ANIMALS_MAP[CODE]].replace(9, np.nan)
-    # data[ORIGIN_MAP[CODE]] = data[ORIGIN_MAP[CODE]].replace([98, 99], np.nan)
-    # data[HEALTH_MAP[CODE]] = data[HEALTH_MAP[CODE]].replace(9, np.nan)
-
-    # Get frequencies and percentages for animals phobia
-    animals_freq = data[ANIMALS_MAP[CODE]].value_counts(sort=False, dropna=False)
-    animals_percent = data[ANIMALS_MAP[CODE]].value_counts(sort=False, dropna=False, normalize=True)
-    label_print(animals_freq, animals_percent, ANIMALS_MAP)
-
-    # Get frequencies and percentages for origins distribution
-    origins_freq = data[ORIGIN_MAP[CODE]].value_counts(dropna=False).head(10)
-    origins_percent = data[ORIGIN_MAP[CODE]].value_counts(dropna=False, normalize=True).head(10)
-    label_print(origins_freq, origins_percent, ORIGIN_MAP)
-
-    # Get frequencies and percentages for perceived health distribution
-    health_freq = data[HEALTH_MAP[CODE]].value_counts(sort=True, dropna=False)
-    health_percent = data[HEALTH_MAP[CODE]].value_counts(sort=True, dropna=False, normalize=True)
-    label_print(health_freq, health_percent, HEALTH_MAP)
-
+    data[ORIGIN_MAP[CODE]] = data[ORIGIN_MAP[CODE]].replace([98, 99], np.nan)
+    data[HEALTH_MAP[CODE]] = data[HEALTH_MAP[CODE]].replace(9, np.nan)
 
     # Get percentages / frequencies of AP by origin
+    
     # Get frequencies by origin
-    origins = data[ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=False)
+    # origins = data[ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=False)  # With NaN
+    origins = data[ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=True)  # Without NaN
     # print('\nOrigins frequencies')
     # print(origins.rename(ORIGIN_MAP[VALUES]))
 
     # print('\nOrigins with AP frequencies')
     # Get origin frequencies based on the condition that respondents have animal phobia
     condition = data[ANIMALS_MAP[CODE]] == 1
-    origins_with_ap = data[condition][ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=False)
-    print(origins_with_ap.rename(ORIGIN_MAP[VALUES]))
+    # origins_with_ap = data[condition][ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=False)  # With NaN
+    origins_with_ap = data[condition][ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=True)  # Without NaN
+    # print(origins_with_ap.rename(ORIGIN_MAP[VALUES]))
 
     # Create a new dataframe out of these two frequency series
     origins_df = origins.rename(ORIGIN_MAP[VALUES]).to_frame(name='ORIGCOUNTS')
@@ -89,14 +76,14 @@ def main():
 
     # Create a new column in this new df to store percentages
     origins_df['APPERCENT'] = origins_df['ORIGAPCOUNTS'] / origins_df['ORIGCOUNTS']
-    print('\nOrigins vs. AP dataframe')
-    print(origins_df.sort_values(by=['APPERCENT'], ascending=False))
-    print('\nPrint only percentages')
-    print(origins_df['APPERCENT'].sort_values(ascending=False))
-
+    # print('\nOrigins vs. AP dataframe')
+    # print(origins_df.sort_values(by=['APPERCENT'], ascending=False))
+    # print('\nPrint only percentages')
+    # print(origins_df['APPERCENT'].sort_values(ascending=False))
 
 
     # Create mixed vs. pure variable
+
     # Create new columns to store recoded values for different kinds of specific phobias
     for phobia in ALL_SPECIFIC_PHOBIAS:
         data[phobia[CODE] + '_NEW'] = data[phobia[CODE]].replace([2, 9], 0)
@@ -115,44 +102,44 @@ def main():
     has_ap = data[ANIMALS_MAP[CODE]] == 1
     has_pure_ap = data[APPUREMIXED] == 1
     origins_pure_ap = data[(has_ap & has_pure_ap)][ORIGIN_MAP[CODE]].value_counts(sort=False, dropna=False)
-    print('\nFrequencies of pure animal phobia by origin')
-    print(origins_pure_ap.rename(ORIGIN_MAP[VALUES]))
+    # print('\nFrequencies of pure animal phobia by origin')
+    # print(origins_pure_ap.rename(ORIGIN_MAP[VALUES]))
 
     # Adding pure AP frequencies to my new dataframe
     origins_df['NUMWITHPUREAP'] = origins_pure_ap.rename(ORIGIN_MAP[VALUES])
     origins_df['PUREAPPERCENTALL'] = origins_df['NUMWITHPUREAP'] / origins_df['ORIGCOUNTS']
     origins_df['PUREAPPERCENTWITHIN'] = origins_df['NUMWITHPUREAP'] / origins_df['ORIGAPCOUNTS']
-    print('\nOrigins vs. AP dataframe - UPDATED')
-    print(origins_df.sort_values(by=['APPERCENT'], ascending=False))
+    # print('\nOrigins vs. AP dataframe - UPDATED')
+    # print(origins_df.sort_values(by=['APPERCENT'], ascending=False))
+    # print(origins_df.sort_values(by=['ORIGCOUNTS'], ascending=False))
+
+    # Make a subset for origins with 400 or more respondents
+    subset_orig_gte_400 = origins_df[origins_df['ORIGCOUNTS'] >= 400].copy()
+    # print('Origins subset (gte 400 respondents)')
+    # print(subset_orig_gte_400.sort_values(by=['APPERCENT'], ascending=False))
+    #
+    # print('\nOrigins subset (gte 400 respondents) - (colnum, rownum)')
+    # print(subset_orig_gte_400.shape)
+    print('\nAPPERCENT solo')
+    print(subset_orig_gte_400['APPERCENT'].sort_values(ascending=False))
 
 
-    # Making a subset for only those with animal phobia
-    # raw_subset = data[(data[ANIMALS_MAP[CODE]] == 1)]
-    #
-    # subset = raw_subset.copy()
-    #
-    # # Distinguish pure and mixed AP cases
-    #
-    # # Replacing 'NO/Unknown' with zero in all specific phobias to count only YES responses
-    # for phobia in SPECIFIC_PHOBIAS:
-    #     subset[phobia[CODE]] = subset[phobia[CODE]].replace([2, 9], 0)
-    #
-    # # Create a new variable that sums up values across all specific phobias if AP == 1. Suggestion:
-    # # Those > 1 are mixed
-    # subset[APPUREMIXED] = subset[ANIMALS_MAP[CODE]] + subset[HEIGHTS_MAP[CODE]] + subset[THUNDER_MAP[CODE]]\
-    #                       + subset[WATER_MAP[CODE]] + subset[FLYING_MAP[CODE]] + subset[CROWD_MAP[CODE]]\
-    #                       + subset[CLOSED_MAP[CODE]] + subset[BLOOD_MAP[CODE]] + subset[TRAVELING_MAP[CODE]]\
-    #                       + subset[DENTIST_MAP[CODE]] + subset[HOSPITAL_MAP[CODE]] + subset[OUTSIDE_MAP[CODE]]
-    # print('\nValues distribution for new APPUREMIXED variable')
-    # print(pd.concat(dict(Frequencies=subset[APPUREMIXED].value_counts(sort=True, dropna=False),
-    #                      Percentages=subset[APPUREMIXED].value_counts(sort=True, dropna=False, normalize=True)), axis=1))
-    #
-    # # Replacing all values >1 with zero to get clear binary picture
-    # print('\nValues distribution for modified APPUREMIXED (1 - Pure, 0 - Not Pure, i.e. mixed)')
-    # condition_for_replace = subset[APPUREMIXED] > 1
-    # subset.loc[condition_for_replace, APPUREMIXED] = 0
-    # print(pd.concat(dict(Frequencies=subset[APPUREMIXED].value_counts(sort=True, dropna=False),
-    #                      Percentages=subset[APPUREMIXED].value_counts(sort=True, dropna=False, normalize=True)), axis=1))
+    # Processing health perception
+
+    # Get percentages for perceived health distribution (for all)
+    health_percent = data[HEALTH_MAP[CODE]].value_counts(sort=False, dropna=True, normalize=True)
+
+    # health perception vs. animal phobia
+    health_ap_percent = data[data[ANIMALS_MAP[CODE]] == 1][HEALTH_MAP[CODE]].value_counts(sort=False, dropna=True, normalize=True)
+
+    # health perception vs. pure animal phobia
+    health_pure_ap_percent = data[(has_ap & has_pure_ap)][HEALTH_MAP[CODE]].value_counts(sort=False, dropna=True,
+                                                                                          normalize=True)
+
+    print('\nCompared distribution percentages for Perceived Health')
+    print(pd.concat(dict(Dataset=health_percent.rename(HEALTH_MAP[VALUES]),
+                         AnimalPhobia=health_ap_percent.rename(HEALTH_MAP[VALUES]),
+                         PureAnimalPhobia=health_pure_ap_percent.rename(HEALTH_MAP[VALUES])), axis=1))
 
 
 if __name__ == '__main__':
