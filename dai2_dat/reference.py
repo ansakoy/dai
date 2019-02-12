ccb9d260994a1d52e65e67643b9365e6b3571dc6
ansakoy <ansakoy@gmail.com>
2019-02-12 12:50:47 +0300

    assignment2

---
+++ dai2_dat/reference.py
@@ -0,0 +1,199 @@
+'''
+Mapping coded values and their meanings based on the dataset codebook
+'''
+
+
+ANIMALS_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+
+HEALTH_VALUES = {
+    1: 'Excellent',
+    2: 'Very good',
+    3: 'Good',
+    4: 'Fair',
+    5: 'Poor',
+    9: 'Unknown'
+}
+
+
+ORIGINS_VALUES = {
+    1: "African American (Black, Negro, or Afro-American)",
+    2: "African (e.g., Egyptian, Nigerian, Algerian)",
+    3: "American Indian (Native American)",
+    4: "Australian, New Zealander",
+    5: "Austrian",
+    6: "Belgian",
+    7: "Canadian",
+    8: "Central American (e.g., Nicaraguan, Guatemalan)",
+    9: "Chicano",
+    10: "Chinese",
+    11: "Cuban",
+    12: "Czechoslovakian",
+    13: "Danish",
+    14: "Dutch",
+    15: "English",
+    16: "Filipino",
+    17: "Finnish",
+    18: "French",
+    19: "German",
+    20: "Greek",
+    21: "Guamanian",
+    22: "Hungarian",
+    23: "Indian, Afghanistani, Pakistani",
+    24: "Indonesian",
+    25: "Iranian",
+    26: "Iraqi",
+    27: "Irish",
+    28: "Israeli",
+    29: "Italian",
+    30: "Japanese",
+    31: "Jordanian",
+    32: "Korean",
+    33: "Lebanese",
+    34: "Malaysian",
+    35: "Mexican",
+    36: "Mexican-American",
+    37: "Norwegian",
+    38: "Polish",
+    39: "Puerto Rican",
+    40: "Russian",
+    41: "Scottish",
+    42: "Samoan",
+    43: "South American (e.g., Brazilian, Chilean, Columbian)",
+    44: "Spanish (Spain) , Portugese",
+    45: "Swedish",
+    46: "Swiss",
+    47: "Taiwanese",
+    48: "Turkish",
+    49: "Vietnamese",
+    50: "Welsh",
+    51: "Yugoslavian",
+    52: "Other Asian (Thai, Laotian, Cambodian, Burmese)",
+    53: "Other Caribbean or West Indian (Spanish Speaking)",
+    54: "Other Caribbean or West Indian (Non-Spanish Speaking)",
+    55: "Other Eastern European (Romanian, Bulgarian, Albanian)",
+    56: "Other Middle Eastern (Saudi Arabian, Kuwaiti, Qatari, Syrian, Omani)",
+    57: "Other Pacific Islander (e.g., Okinawan)",
+    58: "Other Spanish",
+    98: "Other",
+    99: "Unknown"
+}
+
+REGIONS = {
+    1: 'Africa',
+     3: '[Native Americans]',
+     8: 'Latin America',
+     11: 'Latin America',
+     14: 'Central Europe',
+     15: 'Western Europe',
+     18: 'Western Europe',
+     19: 'Central Europe',
+     27: 'Western Europe',
+     29: 'Southern Europe',
+     35: 'Latin America',
+     36: 'Latin America',
+     37: 'Northern Europe',
+     38: 'Central Europe',
+     39: 'Latin America',
+     40: 'Eastern Europe',
+     41: 'Western Europe',
+     43: 'Latin America',
+     44: 'Southern Europe',
+     45: 'Northern Europe'
+}
+
+
+REGIONS_ORIGINS = {
+    "African American (Black, Negro, or Afro-American)": "Africa",
+    "American Indian (Native American)": "[Native Americans]",
+    "South American (e.g., Brazilian, Chilean, Columbian)": "Latin America",
+    "Central American (e.g., Nicaraguan, Guatemalan)": "Latin America",
+    "Puerto Rican": "Latin America",
+    "Dutch": "Central Europe",
+    "French": "Western Europe",
+    "Spanish (Spain) , Portugese": "Southern Europe",
+    "Italian": "Southern Europe",
+    "Irish": "Western Europe",
+    "Scottish": "Western Europe",
+    "English": "Western Europe",
+    "Cuban": "Latin America",
+    "Mexican-American": "Latin America",
+    "Norwegian": "Northern Europe",
+    "Mexican": "Latin America",
+    "German": "Central Europe",
+    "Swedish": "Northern Europe",
+    "Polish": "Central Europe",
+    "Russian": "Eastern Europe"
+}
+
+
+# Other Specific Phobias
+HEIGHTS_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+THUNDER_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+WATER_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+FLYING_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+CROWD_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+CLOSED_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+BLOOD_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+TRAVELING_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+DENTIST_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+HOSPITAL_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
+
+OUTSIDE_VALUES = {
+    1: 'Yes',
+    2: 'No',
+    9: 'Unknown'
+}
